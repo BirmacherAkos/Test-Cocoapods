92591c6bcbbdca1c6d57e2da7d127e932e41e4e9
Akos Birmacher <akos.birmacher@bitrise.io>
2020-05-11 15:14:52 +0200

    Initial Commit

---
+++ Test-Cocoapods/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Test-Cocoapods
+//
+//  Created by Akos Birmacher on 2020. 05. 11..
+//  Copyright © 2020. Akos Birmacher. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
